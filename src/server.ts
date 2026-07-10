8f7379e84513f24e7957ab5f2ad855d80172ef54
fawzan saeed <fauzdasoodais@gmail.com>
2026-07-10 13:45:00 +0300

    latest

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import './app';
+import 'dotenv';
+
+
+const PORT = process.env.PORT || 3000;
+
+async function startServer(){
+    await prisma
+}
---
+++ src/utils/prsima.ts
@@ -0,0 +1,8 @@
+import { PrismaClient } from '@prisma/client'
+import { log } from 'node:console';
+
+const globalForPrisma = globalThis as unknown as {prisma: PrismaClient};
+
+export const prisma =  globalForPrisma.prisma || new PrismaClient({
+    log: process
+}) 
